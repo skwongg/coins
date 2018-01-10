e9ebb809ef67329154e022c3baf0b61757d044a3
silas kwong <silaskwong1@gmail.com>
2018-01-09 22:46:24 -0800

    anchoring commit added basic coin model no validation

--- coin/models.py
+++ coin/models.py
@@ -1,1 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
+
+class Coin(models.Model):
+    name=models.CharField(max_length=255)
+    ticker=models.CharField(max_length=10)
+    price=models.DecimalField()
+    btc_price=models.DecimalField()
