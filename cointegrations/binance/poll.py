b9280e8e990a74d211ac787ce36468d4d49787e0
silas kwong <silaskwong1@gmail.com>
2018-01-27 17:51:02 -0800

    refactor to abstract main pairs and trading pairs into methods so future changes can be easily encapsulated

--- cointegrations/binance/poll.py
+++ cointegrations/binance/poll.py
@@ -7,8 +7,11 @@ BINAN_SECRET_KEY=os.environ.get("BINAN_SECRET_KEY")
 BINAN_BASE_URL=os.environ.get("BINAN_BASE_URL")
 
 class BinancePoll(Coin):
+    btc_price = ''
+    maincoins=dict()
+
     def pollo_world(self):
-        return self.get_all_coins()
+        return self.update_all_coins()
 
     def check_heartbeat(self):
         r = requests.get("{0}/ping".format(BINAN_BASE_URL))
@@ -17,66 +20,66 @@ class BinancePoll(Coin):
         else:
             raise
 
-    def get_all_coins(self):
-        main_coins = dict()
-        r = requests.get(BINAN_BASE_URL + "/ticker/allPrices")
-        all_coins = r.json()
+    def find_btc(self, all_coins):
         for coin in all_coins:
             if coin['symbol'] == 'BTCUSDT':
-                big_btc = float(coin['price'])
-                break
-        if not big_btc:
-            raise
+                return float(coin['price'])
+        raise ValueError('Bitcoin USD pair not found.')
 
+    def write_basepairs(self, coin_obj):
+        if 'USDT' in coin_obj['symbol']:
+            try:
+                basepair=Coin.objects.get(pair=coin_obj['symbol'])
+            except:
+                basepair=Coin(pair=coin_obj['symbol'])
+            ticker = coin_obj['symbol'].split('USDT')[0]
+            price = coin_obj['price']
+            basepair.ticker = ticker
+            basepair.name = ticker
+            basepair.price = price
+            basepair.btc_price = self.btc_price
+            basepair.save()
+            return ticker, price
+        else:
+            return False
+
+    def write_tradepairs(self, coin_obj):
+        if 'USDT' in coin_obj['symbol']:
+            return False
+        else:
+            for maincoin in self.maincoins.keys():
+                if maincoin in coin_obj['symbol'] and coin_obj['symbol'].index(maincoin) > 0:
+                    cutoff = coin_obj['symbol'].index(maincoin)
+                    ticker = coin_obj['symbol'][0:cutoff]
+                    in_terms_of = coin_obj['symbol'][cutoff:]
+                    price = float(self.maincoins[in_terms_of]) * float(coin_obj['price'])
+
+                    try:
+                        cryptopair = Coin.objects.get(pair=coin_obj['symbol'])
+                    except:
+                        cryptopair = Coin(pair=coin_obj['symbol'])
+                    cryptopair.ticker = ticker
+                    cryptopair.name = ticker
+                    cryptopair.price = price
+                    cryptopair.btc_price = (price / self.btc_price)
+                    cryptopair.save()
+
+                    print(price, ticker, coin_obj['price'], in_terms_of)
+                    print("Coin {0} saved successfully.".format(ticker))
+                    print ("*" * 100)
+                else:
+                    continue
+
+    def update_all_coins(self):
+        all_coins = requests.get(BINAN_BASE_URL + "/ticker/allPrices").json()
+        self.btc_price = self.find_btc(all_coins)
+        #BASE COINS: BTC, ETH, LTC, NEO, BNB
         for coin in all_coins:
-            if 'USDT' in coin['symbol']:
-                try:
-                    basepair = Coin.objects.get(pair=coin['symbol'])
-                except:
-                    basepair = Coin(pair=coin['symbol'])
-                ticker = coin['symbol'].split('USDT')[0]
-                price = coin['price']
-                basepair.ticker = ticker
-                basepair.name = ticker
-                basepair.price = price
-                basepair.btc_price = (float(price) / big_btc)
-                basepair.save()
-
-                ##turn into dictionary entry
-                main_coins[ticker] = price
-
+            if self.write_basepairs(coin):
+                self.maincoins[self.write_basepairs(coin)[0]] = self.write_basepairs(coin)[1]
         ##TRADING pairs
         for coin in all_coins:
-            if 'USDT' in coin['symbol']:
-                continue
-            else:
-                for maincoin in main_coins.keys():
-                    if maincoin in coin['symbol']:
-                        if coin['symbol'].index(maincoin) > 0:
-                            cutoff = coin['symbol'].index(maincoin)
-                            ticker = coin['symbol'][0:cutoff]
-                            in_terms_of = coin['symbol'][cutoff:]
-                            price = float(main_coins[in_terms_of]) * float(coin['price'])
-
-
-                            # print(ticker, in_terms_of)
-
-                            try:
-                                cryptopair = Coin.objects.get(pair=coin['symbol'])
-                            except:
-                                cryptopair = Coin(pair=coin['symbol'])
-                            cryptopair.ticker = ticker
-                            cryptopair.name = ticker
-                            cryptopair.price = price
-                            cryptopair.btc_price = (price / big_btc)
-                            cryptopair.save()
-
-                            print(price, ticker, coin['price'], in_terms_of)
-                            print("Coin {0} saved successfully.".format(ticker))
-                            print ("*" * 100)
-
-
-                # print (coin['symbol'], coin['price'])
+            self.write_tradepairs(coin)
         return all_coins, len(all_coins)
 
     def update_coins(self):
