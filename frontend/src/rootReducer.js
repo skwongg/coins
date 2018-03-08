33ae57f275380ca95d41965b08f55cc317bb28d0
silas kwong <silaskwong1@gmail.com>
2018-03-07 17:22:28 -0800

    add coins page, coins reducer, coins actions

--- frontend/src/rootReducer.js
+++ frontend/src/rootReducer.js
@@ -1,6 +1,6 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import user from './reducers/user'
-
+import coins from './reducers/coins'
 export default combineReducers({
   user
 });
