3397cb0458faff19c7b4193b322ecbd656296b57
silas kwong <silaskwong1@gmail.com>
2018-04-11 22:55:16 -0700

    cassandra model hooked up to cassandra, needs hardening, but default behavior for time series data looks good

--- cointegrations/cassandra/coins.py
+++ cointegrations/cassandra/coins.py
@@ -1,16 +1,25 @@
 from cassandra.cluster import Cluster
-from cassandra.policies import DCAwareRoundRobinPolicy
-from cassandra.cqlengine.models import Model, columns
-
+import uuid
+from cassandra.cqlengine import columns
+from cassandra.cqlengine import connection
+from datetime import datetime
+from cassandra.cqlengine.management import sync_table
+from cassandra.cqlengine.models import Model
 cluster=Cluster()
 
 class Coin(Model):
-    __keyspace__ = "cassy"
-    __table_name__ = "coins"
-    id = columns.UUID(primary_key=True)
-    name = columns.Text()
-    ticker = columns.Text()
-    pair = columns.Text()
-    icon_url = columns.Text()
-    price = columns.Decimal()
-    btc_price = columns.Decimal()
+    coin_id=columns.UUID(primary_key=True, default=uuid.uuid4)
+    name=columns.Text(required=True)
+    ticker=columns.Text(required=True)
+    pair=columns.Text(required=True)
+    icon_url=columns.Text(required=False)
+    price=columns.Decimal(required=False)
+    btc_price=columns.Decimal(required=False)
+    created_at=columns.DateTime()
+
+
+connection.setup(['127.0.0.1'], "cassy", protocol_version=3)
+sync_table(Coin)
+
+
+##Cassandra coin model syncs to default cassandra connection under cassy keyspace.
