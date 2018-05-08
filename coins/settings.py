bca924b937b5c1fb2eddd08fc3e63058da981f0f
silas kwong <silaskwong1@gmail.com>
2018-05-07 20:20:59 -0700

    dotenv removed in favor of python-dotenv

--- coins/settings.py
+++ coins/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from os.path import join, dirname
-from dotenv import Dotenv
-dotenv = Dotenv(join(dirname(__file__), '.env'))
-os.environ.update(dotenv)
+from dotenv import load_dotenv
+dotenv = load_dotenv(dotenv_path = join(dirname(__file__), '.env'))
+# os.environ.update(dotenv)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
